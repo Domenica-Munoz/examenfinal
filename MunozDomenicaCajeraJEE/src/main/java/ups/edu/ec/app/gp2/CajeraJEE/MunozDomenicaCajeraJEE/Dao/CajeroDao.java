470fa93b63082a2b4ae0fc022fc90901db61456d
Domenica <mmunozo2@est.ups.edu.ec>
2021-02-05 08:05:47 -0500

    inicio

---
+++ MunozDomenicaCajeraJEE/src/main/java/ups/edu/ec/app/gp2/CajeraJEE/MunozDomenicaCajeraJEE/Dao/CajeroDao.java
@@ -0,0 +1,7 @@
+package ups.edu.ec.app.gp2.CajeraJEE.MunozDomenicaCajeraJEE.Dao;
+
+public class CajeroDao {
+	
+   public
+
+}
---
+++ MunozDomenicaCajeraJEE/src/main/java/ups/edu/ec/app/gp2/CajeraJEE/MunozDomenicaCajeraJEE/util/Cajero.java
@@ -0,0 +1,33 @@
+package ups.edu.ec.app.gp2.CajeraJEE.MunozDomenicaCajeraJEE.util;
+
+public class Cajero {
+	
+ private String cedula;
+ private String celular;
+ private boolean valor_recarga;
+ 
+ 
+ 
+public String getCedula() {
+	return cedula;
+}
+public void setCedula(String cedula) {
+	this.cedula = cedula;
+}
+public String getCelular() {
+	return celular;
+}
+public void setCelular(String celular) {
+	this.celular = celular;
+}
+public boolean isValor_recarga() {
+	return valor_recarga;
+}
+public void setValor_recarga(boolean valor_recarga) {
+	this.valor_recarga = valor_recarga;
+}
+ 
+ 
+ 
+
+}
